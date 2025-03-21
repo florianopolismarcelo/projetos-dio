c81acf04821630fad700db198089c4968475ce0c
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-03-21 16:53:20 -0300

    implementando o listener de hitbox

--- 001 - criando-um-jogo-do-detona-ralph-com-javascript/src/scripts/engine.js
+++ 001 - criando-um-jogo-do-detona-ralph-com-javascript/src/scripts/engine.js
@@ -1,13 +1,15 @@
 const state = {
   view: {
     squares: document.querySelectorAll(".square"),
-    enemy: document.querySelectorAll(".enemy"),
+    enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
   },
   values: {
     timerId: null,
-    gameVelocity: 1000
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
   },
 };
 
@@ -18,16 +20,26 @@ function randomSquare() {
   let randomNumber = Math.floor(Math.random() * 9);
   let randomSquare = state.view.squares[randomNumber];
   randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
 }
 function moveEnemy() {
   state.values.timerId = setInterval(randomSquare.values.gameVelocity);
 }
 function addListenerHitBox() {
-  state.view.squares.forEach((square) => {});
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+      }
+    })
+  });
 }
 
 function initialize() {
-  randomSquare();
+  moveEnemy()
+  addListenerHitBox()
 }
 
 initialize();
