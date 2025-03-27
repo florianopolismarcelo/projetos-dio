20d7a2794467dd9e0f88ce3c003fe972e43049f7
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-03-27 15:28:49 -0300

    criando condições de vitória

--- 002 - memory-game/src/scripts/engine.js
+++ 002 - memory-game/src/scripts/engine.js
@@ -33,7 +33,21 @@ function handleClick(){
   if(openCards.length == 2) {
     setTimeout(checkMatch, 500);
   }
+  console.log(openCards);
 }
+
 function checkMatch() {
-  
+  if (openCards[0].innerHTML ===
+    openCards[1].innerHTML) {
+      openCards[0].classList.add("boxMatch");
+      openCards[1].classList.add("boxMatch");
+    } else {
+      openCards[0].classList.remove("boxOpen");
+      openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+      alert("Você venceu! Jogue outra vez... ")
+    }
 }
